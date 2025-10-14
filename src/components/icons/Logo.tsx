57c045978c222ef40e902b9361fe60966b1367c5
sohail pashe <sohailpashe@gmail.com>
2025-10-14 21:31:05 +0000

    change the app icon to  stylish smooth flow relatable icon or small flow

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -11,10 +11,8 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M15 6v12" />
-    <path d="M15 6H9a3 3 0 0 0-3 3v0a3 3 0 0 0 3 3h4" />
-    <path d="M15 12H9" />
-    <path d="M5 6v12" />
+    <path d="M4 12h4a4 4 0 0 1 4 4v4" />
+    <path d="M20 12h-4a4 4 0 0 0-4-4V4" />
   </svg>
 );
 
