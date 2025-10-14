12cac1c3c4a88495b71148f18096cdcf4e084273
sohail pashe <sohailpashe@gmail.com>
2025-10-14 13:31:11 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/pass.ts
+++ src/lib/pass.ts
@@ -54,7 +54,7 @@ export async function generatePassImage(registration: Registration, event: Event
           justifyContent: 'center',
           width: '100%',
           height: '100%',
-          background: '#1e1b4b', // Using a dark indigo color
+          backgroundColor: '#1e1b4b',
         }}
       >
         <EventPass registration={registration} event={event} />
