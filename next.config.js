407f2e8eda2bdb3bfb7f4257d6c667c66acacc29
sohail pashe <sohailpashe@gmail.com>
2025-10-25 13:43:35 +0000

    Ah! That warning is the key:
    Unable to find a valid endpoint for functi

---
+++ next.config.js
@@ -0,0 +1,54 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+      }
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_EMULATORS_ENABLED: process.env.NEXT_PUBLIC_EMULATORS_ENABLED,
+  },
+};
+
+module.exports = nextConfig;
